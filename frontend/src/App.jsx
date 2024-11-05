43dc19cf85a4df0e4b6a300fcf2ccc3cf45b95b5
Mohammed Enthihaj <mohammedenthihaj.20201cei0011@presidencyuniversity.in>
2024-11-05 19:15:33 +0530

    creates all the forntend ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,80 @@
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router";
+import { Navigate } from "react-router-dom";
+import LoadingSpinner from "../components/LodingSipnner";
+import { useAuthStore } from "../store/authStore";
+import EmailVerification from "./pages/EmailVerification";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user, isCheckingAuth } = useAuthStore();
+
+  if (isCheckingAuth) {
+    return <LoadingSpinner />;
+  }
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  if (!user.isVerified) {
+    return <Navigate to="/verify-email" replace />;
+  }
+  return children;
+};
+
+const RedirectAuthenticatedUser = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (isAuthenticated && user.isVerified) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+};
+
+function App() {
+  const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if(isCheckingAuth)return <LoadingSpinner/>
+
+  return (
+    <div
+      className="min-h-screen bg-gradient-to-br from-white to-blue-100 flex items-center justify-center relative overflow-hidden"
+    >
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <RedirectAuthenticatedUser>
+              <SignUpPage />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RedirectAuthenticatedUser>
+              <LoginPage />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route path="/verify-email" element={<EmailVerification />} />
+      </Routes>
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+   <div>Weclom to cab rentals</div>
+  )
+}
+
+export default Home
